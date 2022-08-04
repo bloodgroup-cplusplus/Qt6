38a0dc6302332abb6435b3c41bf109f70ae71f11
Bhushan <tucater10896601@protonmail.com>
2022-08-04 10:00:50 +0530

    adding images to resource file and compiling using qmake

---
+++ QMakeIcon/widget.cpp
@@ -0,0 +1,19 @@
+#include "widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    setWindowTitle(" QMake Hello world");
+    setFixedWidth(500);
+    setFixedHeight(300);
+   // setStyleSheet("background-color:red");
+    //setWindowOpacity(0.4);
+    // we are going to use qicon to set an icon for windows
+    setWindowIcon(QIcon(": /images/photo.jpeg"));
+
+}
+
+Widget::~Widget()
+{
+}
+
